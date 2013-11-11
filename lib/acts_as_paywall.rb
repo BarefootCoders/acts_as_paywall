e2618540574b59ea1baa80045e8183ad40dc478a
Jason Berlinsky <jason@barefootcoders.com>
2013-11-11 02:44:39 +0200

    Default permissible_controllers to an empty set

--- lib/acts_as_paywall.rb
+++ lib/acts_as_paywall.rb
@@ -11,7 +11,7 @@ module ActsAsPaywall
     module ClassMethods
       def acts_as_paywall(options)
         configuration = {
-          permissible_controllers: options.delete(:permissible_controllers),
+          permissible_controllers: options.delete(:permissible_controllers) || [],
           free_views: (options.delete(:free_views) || 5).to_i,
           wall_url: options.delete(:wall_url) || root_url,
           content_view_cookie_key: (options.delete(:content_view_cookie_key) || :content_views).to_sym
